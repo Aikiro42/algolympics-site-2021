9ad05efc131e79760faa3407c8c0f5477a9bb157
Aikiro42 <38589158+Aikiro42@users.noreply.github.com>
2021-03-10 20:09:42 +0800

    Quick commit
    Changed the way index components are spaced

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 
+import SpacedComponents from "../layouts/spaced-components.js"
+
 import Header from "../components/header.js"
 import Footer from "../components/footer.js"
-import BlankContentfiller from "../components/blank-content-filler.js"
 
 import Landing from "../components/index/landing.js"
 import AlgoIntro from "../components/index/algo-intro.js"
@@ -13,26 +14,17 @@ import Sponsors from "../components/index/sponsors.js"
 import "../styles/global.scss"
 
 export default function Home() {
-	var sectionSpacing = "100px";
 	return (
 		<>
 			<Header isIndex={true} />
 			<Landing />
 			<div class="content wrapper incompressible">
-				<BlankContentfiller height="50px" color="none" />
-
-				<AlgoIntro />
-				<BlankContentfiller height={sectionSpacing} color="none" />
-
-				<Numbers />
-				<BlankContentfiller height={sectionSpacing} color="none" />
-
-				<Prizes />
-				<BlankContentfiller height={sectionSpacing} color="none" />
-
-				<Sponsors />
-				<BlankContentfiller height={sectionSpacing} color="none" />
-
+				<SpacedComponents>
+					<AlgoIntro />
+					<Numbers />
+					<Prizes />
+					<Sponsors />
+				</SpacedComponents>
 			</div>
 			<Footer />
 		</>
